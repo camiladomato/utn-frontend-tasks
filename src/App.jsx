168f7a5bae058e0ecaafe203e4cc3a66be482a16
Camila Domato <camiladomato@gmail.com>
2026-06-11 21:36:14 -0300

    add: commit inicial base de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>¡Proyecto Inicializado con Éxito! 🚀</h1>
+      <p>React + Vite configurados a mano sin depender de la caché.</p>
+    </div>
+  )
+}
+
+export default App
